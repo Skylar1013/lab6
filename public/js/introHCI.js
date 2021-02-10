3f7e464d0dbb207a7f6f767933ce54b19f434f7f
Skylar Yi Li <yil067@ucsd.edu>
2021-02-10 18:21:14 +0800

    all

--- public/js/introHCI.js
+++ public/js/introHCI.js
@@ -10,8 +10,8 @@ $(document).ready(function() {
  */
 function initializePage() {
 	$('.project a').click(addProjectDetails);
-
-	// $('#colorBtn').click(randomizeColors);
+	$('.details').click(addProject);
+	$('#colorBtn').click(randomizeColors);
 }
 
 /*
@@ -20,11 +20,31 @@ function initializePage() {
 function addProjectDetails(e) {
 	// Prevent following the link
 	e.preventDefault();
+	
+	$.get('http://localhost:3000/', callback)
+
+
 
 	// Get the div ID, e.g., "project3"
 	var projectID = $(this).closest('.project').attr('id');
 	// get rid of 'project' from the front of the id 'project3'
 	var idNumber = projectID.substr('project'.length);
-
 	console.log("User clicked on project " + idNumber);
+	$('.details').html('foooo');
+}
+
+function addProject(result) {
+	console.log(result);
+ 	var projectHTML = '<a href="#" class="thumbnail">' +
+    '<img src="' + result['image'] + '" class="img">' +
+    '<p>' + result['title'] + '</p>' +
+    '<p><small>' + result['date'] +
+    '</small></p></a>'; 
+
+    $('#project-container').html(projectHTML);
+    $('#project-description').html(result['summary']);
+}
+
+function callback(result){
+	console.log(result);
 }
